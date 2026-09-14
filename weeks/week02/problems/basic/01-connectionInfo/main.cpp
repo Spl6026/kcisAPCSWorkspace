662e92ef80c357dd86b3e796396773994446a76f
spl <sgpolin@gmail.com>
2026-09-14 18:05:39 +0800

    test

---
+++ weeks/week02/problems/basic/01-connectionInfo/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string s;
+    int a = 0;
+    cin >> s >> a;
+    cout << "Device: " << s << endl;
+    cout << "Channel: " << a;
+}
